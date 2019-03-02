36b5e4365df24f0f33024f854d436e69e6398d3d
Taewoong Moon <moondingie5@gmail.com>
2019-03-02 14:29:06 +0900

    ThemeProvider

--- src/App.js
+++ src/App.js
@@ -1,71 +1,50 @@
 import React, { Component, Fragment } from 'react';
-import styled, { createGlobalStyle, css, keyframes } from 'styled-components';
-// import './App.css';  
+import styled, { createGlobalStyle, ThemeProvider } from 'styled-components';
+import themes from './theme';
 
 const GlobalStyle = createGlobalStyle`
   body {
     padding: 0;
     margin: 0;
   }
+  `;
+  
+const Container = styled.div`
+  height: 100vh;
+  width: 100%;
+  background-color: gray;
+`;
+
+const Card = styled.div`
+  backgroud-color: white;
+`;
+
+const Button = styled.button`
+  border-radius: 30px;
+  padding: 25px 15px;
+  background-color: ${props => props.theme.dangerColor}
 `;
 
 class App extends Component {
   render() {
     return (
-      <React.Fragment>
-        <GlobalStyle />
-        <Container>
-          <Button>Hello</Button>
-          <Button danger rotationTime={1}>Hello</Button>
-          <Input />
-          <Anchor href="http://google.com">Go to Google</Anchor>
-        </Container>
-      </React.Fragment>
+      <ThemeProvider theme={themes}>
+        <React.Fragment>
+          <GlobalStyle />
+            <Container>
+            <Form />
+          </Container>
+        </React.Fragment>
+      </ThemeProvider>
     );
   }
 }
 
-const Container = styled.div`
-  height: 100vh;
-  width: 100%;
-  background-color: gray;
-`
+const Form = () => (
+  <Card>
+    <Button>Hello</Button>
+  </Card>
+)
 
-const Input = styled.input`
-  border: 1px solid red;
-`
-
-const Button = styled.button`
-  border-radius: 50px;
-  padding: 5px;
-  min-width: 120px;
-  color: white;
-  font-weight: 600;
-  -webkit-appearance: none;
-  cursor: pointer;
-  &:active,
-  &:focus {
-    outline: none;
-  }
-  background-color: ${props => (props.danger ? 'red' : 'purple')}
-  ${props => {
-    if (props.danger) {
-      return css `animation: ${rotation} ${props.rotationTime}s linear infinite`;
-    }
-  }}
-`;
-
-const Anchor = styled(Button.withComponent('a'))`
-  text-decoration: none;
-`
-
-const rotation = keyframes`
-  from {
-    transform: rotate(0deg);
-  }
-  to {
-    transform: rotate(360deg);
-  }
-`;
 
 export default App;
